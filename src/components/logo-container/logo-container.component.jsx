fa81cd93ab6bf1ed9b5e689d2660909c30b49254
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-04-03 18:55:10 +0200

    add -- logo container component

---
+++ src/components/logo-container/logo-container.component.jsx
@@ -0,0 +1,11 @@
+import "./logo-container.styles.scss";
+
+const LogoContainer = () => {
+  return (
+    <div className="logo-container">
+      <h2 className="logo">LOGO</h2>
+    </div>
+  );
+};
+
+export default LogoContainer;
--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -1,8 +1,10 @@
+import LogoContainer from "../logo-container/logo-container.component";
 import "./sidebar.styles.scss";
 
 const SideBar = () => {
   return (
     <nav className="side-bar">
+      <LogoContainer />
       <h2>attività da completare</h2>
       <h2>attività completate</h2>
     </nav>
