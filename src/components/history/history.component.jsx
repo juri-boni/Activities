f1c0f6652229c34b754a9f5d069b4433773bc8fd
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-04-04 16:02:42 +0200

    add -- history component

--- src/components/history/history.component.jsx
+++ src/components/history/history.component.jsx
@@ -1,7 +1,11 @@
+import ActivitiesSheet from "../activities-sheet/activities-sheet.component";
+import "./history.styles.scss";
+
 const History = () => {
   return (
     <div className="history-container">
-      <h1>history</h1>
+      <h1>Attività Completate</h1>
+      <ActivitiesSheet />
     </div>
   );
 };
