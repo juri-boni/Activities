c03519be4b099bd7d917066fe54baa71be7453ff
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-04-04 14:53:25 +0200

    routing -- finished routing implementation

---
+++ src/components/history/history.component.jsx
@@ -0,0 +1,9 @@
+const History = () => {
+  return (
+    <div className="history-container">
+      <h1>history</h1>
+    </div>
+  );
+};
+
+export default History;
--- src/components/main-screen/main-screen.component.jsx
+++ src/components/main-screen/main-screen.component.jsx
@@ -1,12 +1,17 @@
 import NavBar from "../navbar/navbar.component";
 import Activities from "../activities/activities.component";
+import History from "../history/history.component";
+import { useLocation } from "react-router-dom";
 import "./main-screen.styles.scss";
 
 const MainScreen = () => {
+  const location = useLocation();
+
   return (
     <div className="main-screen-container">
       <NavBar />
-      <Activities />
+      {location.pathname === "/todos" && <Activities />}
+      {location.pathname === "/completed" && <History />}
     </div>
   );
 };
