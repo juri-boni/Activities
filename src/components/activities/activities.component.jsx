f715e09ed9c9b034bea1d1bfbbe8fde7c72885b2
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-04-05 11:24:06 +0200

    flow -- working on pagination

--- src/components/activities/activities.component.jsx
+++ src/components/activities/activities.component.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
+import { ActivitiesContext } from "../../context/activities.context";
 import ActivitiesSheet from "../activities-sheet/activities-sheet.component";
 import NewActivitiesForm from "../new-activities-form/new-activities-form.component";
 import Pagination from "../pagination/pagination.component";
@@ -7,7 +8,7 @@ import Pagination from "../pagination/pagination.component";
 import "./activities.styles.scss";
 
 const Activities = ({ todos }) => {
-  const [activitiesCurrentPage, setActivitiesCurrentPage] = useState(1);
+  const { activitiesCurrentPage } = useContext(ActivitiesContext);
   const [tasksPerPage, setTasksPerPage] = useState(5);
   const indexOfLastTask = activitiesCurrentPage * tasksPerPage;
   const indexOfFirstTask = indexOfLastTask - tasksPerPage;
--- src/components/history/history.component.jsx
+++ src/components/history/history.component.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import HistorySheet from "../history-sheet/history-sheet.component";
 import Pagination from "../pagination/pagination.component";
 import "./history.styles.scss";
+import { ActivitiesContext } from "../../context/activities.context";
 
 const History = ({ completes }) => {
-  const [historyCurrentPage, setHistoryCurrentPage] = useState(1);
+  const { historyCurrentPage } = useContext(ActivitiesContext);
   const [tasksPerPage, setTasksPerPage] = useState(5);
   const indexOfLastTask = historyCurrentPage * tasksPerPage;
   const indexOfFirstTask = indexOfLastTask - tasksPerPage;
