0670d881e239ce4d06fd68264f794235d399df26
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-04-13 08:22:49 +0200

    implement -- formfields functionalities to create new activity

--- src/components/new-activities-form/new-activities-form.component.jsx
+++ src/components/new-activities-form/new-activities-form.component.jsx
@@ -1,21 +1,61 @@
-import { Fragment } from "react";
+import { useState, Fragment } from "react";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 import "./new-activities-form.styles.scss";
 
+const defaultFormFields = {
+  task: "",
+  operator: "",
+};
+
 const NewActivitiesForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { task, operator } = formFields;
+  console.log(task, operator);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = () => console.log("hello");
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
     <Fragment>
       <h2 className="new-activities-title">NUOVA ATTIVITA'</h2>
       <div className="new-activities-container">
-        <div className="new-activities-form-container">
-          <FormInput label="Descrizione"></FormInput>
-          <FormInput label="Operatore"></FormInput>
-        </div>
-        <div className="new-activities-buttons-container">
-          <Button>Annulla</Button>
-          <Button buttonType="save">Salva</Button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="new-activities-form-container">
+            <FormInput
+              label="Descrizione"
+              type="text"
+              required
+              onChange={handleChange}
+              name="task"
+              value={task}
+            ></FormInput>
+            <FormInput
+              label="Operatore"
+              type="text"
+              required
+              onChange={handleChange}
+              name="operator"
+              value={operator}
+            ></FormInput>
+          </div>
+          <div className="new-activities-buttons-container">
+            <Button type="button" onClick={resetFormFields}>
+              Annulla
+            </Button>
+            <Button type="submit" buttonType="save">
+              Salva
+            </Button>
+          </div>
+        </form>
       </div>
     </Fragment>
   );
--- src/context/activities.context.js
+++ src/context/activities.context.js
@@ -3,14 +3,13 @@ import { getActivities } from "../utils/requests";
 
 export const ActivitiesContext = createContext({
   activities: [],
-  filteredActivities: [],
   currentPage: 1,
   elementsPerPage: 5,
 });
 
 export const ActivitiesProvider = ({ children }) => {
   const [activities, setActivities] = useState([]);
-  // const [filteredActivities, setFilteredActivities] = useState(activities);
+
   const [currentPage, setCurrentPage] = useState(1);
   const [elementsPerPage, setElementsPerPage] = useState(5);
 
